a747c55f360bd9f607dfbb2a690b6d7fd74b672e
shreyas <shreyashraut8@gmail.com>
2025-08-29 23:48:02 +0530

    Setup the file structure of the project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div>
+      <p className="text-red-600">Hello, Next.js!</p>
+      <Button>Click Me</Button>
+    </div>
+  );
+}
