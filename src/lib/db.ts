a873a1575ee203aeecda94a3bf5b95158f23bd41
shreyas <shreyashraut8@gmail.com>
2025-09-02 16:17:00 +0530

    Changes done in db.ts for vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,13 @@
-import { Pool } from 'pg';
+import { Pool } from "pg";
 
 // Ensure the DATABASE_URL environment variable is set
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL environment variable is not set.');
+  throw new Error("DATABASE_URL environment variable is not set.");
 }
 
 const pool: Pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, // ✅ important for Vercel
   max: 15, // Set the maximum number of connections in the pool
 });
 
